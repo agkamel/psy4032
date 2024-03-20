d1c7a5b6a45468063bc1c4f927e0f86a2ad8d8cd
agkamel <alexandre.gellen.kamel@gmail.com>
2024-03-19 20:09:10 -0400

    Create a new correlated dataframe

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,7 +4,7 @@ set.seed(41)
 
 age <- agk::create_discrete(mean = 35, sd = 15)
 sexe <- agk::create_nominal(points = 2, levels = c("h", "f"))
-presence <- agk::create_dichotomic(levels = c("present", "absent"))
+presence <- agk::create_dichotomous(levels = c("present", "absent"))
 vitesse <- stats::rnorm(20, mean = 25, sd = 5)
 anxiete <- agk::create_ordinal(points = 20)
 depression <- agk::create_ordinal(points = 40)
@@ -36,6 +36,23 @@ tableau <- tibble::tibble(
 )
 
 usethis::use_data(tableau, overwrite = TRUE)
+
+tableau500 <- agk::create_dataset(n = 500, v = 10)
+
+tableau500 |> dplyr::mutate(
+  id = 1:nrow(tableau500),
+  age = agk::rescale(v01, mean = 35, sd = 15) |> agk::as_discrete(),
+  sexe = agk::as_nominal(v02, points = 2, levels = c("h", "f")),
+  presence = agk::as_dichotomous(v03, points = 2, levels = c("present", "absent")),
+  vitesse = agk::rescale(v04, mean = 25, sd = 5) |> agk::as_continuous(),
+  anxiete = agk::as_ordinal(v05, points = 20),
+  depression = agk::as_ordinal(v06, points = 40),
+  couleur = agk::as_nominal(v07, levels = c("bleu", "rouge", "vert")),
+  score01 = agk::rescale(v08, mean = 50, sd = 10) |> agk::as_discrete(),
+  score02 = agk::rescale(v09, mean = 70, sd = 7) |> agk::as_discrete()
+) |> dplyr::select(!c("v01", "v02", "v03", "v04", "v05", "v06", "v07", "v08", "v09", "v10"))
+
+usethis::use_data(tableau500, overwrite = TRUE)
 
 data_context <- dplyr::tribble(
   ~var, ~type, ~points, ~mean, ~sd, ~levels,
@@ -72,8 +89,74 @@ data_context <- dplyr::tribble(
 # [9] "https://psycnet-apa-org.proxy.bibliotheques.uqam.ca/thesaurus/item?page=692"
 # [10] "https://psycnet-apa-org.proxy.bibliotheques.uqam.ca/thesaurus/item?page=283"
 
+set.seed(41)
+
+ndataset <- 3
+possible_observation <- 200:1000
+possible_variable <- 6:10
+possible_functions <- c(cont = agk::as_continuous, disc = agk::as_discrete, ord = agk::as_ordinal, nom = agk::as_nominal, dich = agk::as_dichotomous)
+
+for (i in seq(1,ndataset)) {
+  n <- sample(possible_observation, 1)
+  v <- sample(possible_variable, 1)
+  funs <- sample(possible_functions, size = v, replace = TRUE)
+  funs_names <- names(funs)
+
+  #print(funs)
+  #print(funs_names)
+  #print(n)
+  #print(v)
+
+  tb <- agk::create_dataset(n = n,
+                            v = v)
+
+
+  for (i in 1:v) {
+
+    if (names(funs[i]) == "cont") {
+      tb[[i]] <-
+        agk::rescale(tb[[i]],
+                     mean = sample(25:50, 1),
+                     sample(seq(10, 15, by = 0.1), 1)) |>
+        agk::as_continuous()
+      names(tb)[i] <- paste0(names(tb)[i], "_", funs_names[i])
+
+    } else if (names(funs[i]) == "disc") {
+        tb[[i]] <-
+          agk::rescale(tb[[i]],
+                       mean = sample(25:50, 1),
+                       sample(seq(10, 15, by = 0.1), 1)) |>
+          agk::as_discrete()
+        names(tb)[i] <- paste0(names(tb)[i], "_", funs_names[i])
+
+    } else if (names(funs[i]) == "ord") {
+      tb[[i]] <-
+        agk::as_ordinal(tb[[i]], points = sample(7:20, 1))
+      names(tb)[i] <- paste0(names(tb)[i], "_", funs_names[i])
+
+    } else if (names(funs[i]) == "nom") {
+      tb[[i]] <- agk::as_nominal(tb[[i]], points = sample(3:5, 1))
+      names(tb)[i] <- paste0(names(tb)[i], "_", funs_names[i])
+
+    } else if (names(funs[i]) == "dich") {
+      tb[[i]] <- agk::as_dichotomous(tb[[i]])
+      names(tb)[i] <- paste0(names(tb)[i], "_", funs_names[i])
+    }
+
+
+  }
+
+  print(tb)
+
+}
 
 
 
+tb <- agk::create_dataset()
 
+
+
+purrr::map(tb, funs) #|> dplyr::bind_cols()
+
+library(agk)
 
